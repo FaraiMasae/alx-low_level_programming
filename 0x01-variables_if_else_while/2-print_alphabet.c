e3628348fb7c7027c79cbea3eaafbe4f03982941
FaraiMasae <faraishadreckm@gmail.com>
2023-08-12 07:22:05 -0700

    Lowercases

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entrypoint
+ * Description: putschar coding
+ * Return: always zero (success)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+		putchar('\n')
+	return (0);
+}
