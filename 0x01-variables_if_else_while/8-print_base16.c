17a4b1741d5d986d1aaf57338cfa1f230418c389
FaraiMasae <faraishadreckm@gmail.com>
2023-08-12 08:43:04 -0700

    Hexademicals this time

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: hexadecimals now
+ * Return: always zero (a win Mr)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97; j <= 102; j++)
+	{
+		putchar(j);
+	}
+	return (0);
+}
