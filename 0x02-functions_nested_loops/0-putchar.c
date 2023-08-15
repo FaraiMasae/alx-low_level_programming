89d91fc97f98df6f09ae4e154b2fcfa7e278fa2c
FaraiMasae <faraishadreckm@gmail.com>
2023-08-15 12:10:03 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: user defined header
+ * Return: always zero (a win for us)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
