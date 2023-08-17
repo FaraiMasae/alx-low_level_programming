6257099a7a0f9cafe0c361109e67a3a017dfc963
FaraiMasae <faraishadreckm@gmail.com>
2023-08-17 14:18:59 -0700

    Multiplying can be amazing

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - a multiplier
+ * @a: first number
+ * @b: second number
+ * Return: value of a number
+ */
+int mul(int a, int b)
+{
+	int ans = a * b;
+
+	return (ans);
+}
